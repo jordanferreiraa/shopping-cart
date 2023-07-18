0787b10b5dd32cb45d8e9afc86599e8d3a554346
jordanferreiraa <jordanferreira69@gmail.com>
2023-07-18 17:10:18 -0300

    feat: header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
